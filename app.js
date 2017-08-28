f90857d37e2853ea4f3e00e1b199276f00aeef6d
wanpeng <wanpeng19860919@163.com>
2017-08-28 09:29:06 +0800

    <feat>模拟扫码开机流程

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 var mqtt = require('mqtt')
 const DEVICEID = 'yjb0001'
 
+//设备状态
+const IDLE = 0  //空闲
+const OCCUPIED = 1 //使用中
+
+const Device_Info = {
+  deviceId: DEVICEID,
+  status: IDLE,
+}
+
 const CONN_OPTION = {
   clientId: DEVICEID,
   will: {
@@ -15,11 +24,6 @@ const CONN_OPTION = {
 
 var client  = mqtt.connect('mqtt://123.56.216.122:1883', CONN_OPTION)
 
-
-//设备状态
-const IDLE = 0  //空闲
-const OCCUPIED = 1 //使用中
-
 //设备上线
 client.on('connect', function (connack) {
   console.log("connected" )
@@ -28,6 +32,73 @@ client.on('connect', function (connack) {
     time: Date.now(),
   }
   client.publish('online', JSON.stringify(onlineMessage))
+  Device_Info.status = IDLE   //设备状态-->空闲
+
+  var topics = []
+  topics.push('turnOn/' + DEVICEID)  //订阅开机消息
+
+  client.subscribe(topics, function (error) {
+    if(error) {
+      console.log("subscribe error", error)
+    } else {
+      console.log("subscribe success, topics:", topics)
+    }
+  })
+})
+
+function handleTurnOn(message) {
+  var Message = JSON.parse(message.toString())
+  var deviceId = Message.deviceId
+  var socketId = Message.socketId
+
+  if(Device_Info.status === IDLE) {
+    var successMessage = {
+      socketId: socketId,
+      deviceId: deviceId,
+      time: Date.now(),
+      status: OCCUPIED,
+    }
+    setTimeout(function () {
+      client.publish('turnOnSuccess/' + deviceId, JSON.stringify(successMessage), function (error) {
+        if(error) {
+          console.log("publish turnOnSuccess error:", error)
+        } else {
+          Device_Info.status = OCCUPIED //设备状态-->使用中
+          console.log("publish success, topic:", 'turnOnSuccess/' + deviceId)
+        }
+      })
+    }, 1000)
+  } else {
+    var failedMessage = {
+      deviceId: deviceId,
+      time: Date.now(),
+      status: OCCUPIED,
+      message: "设备故障"
+    }
+
+    setTimeout(function () {
+      client.publish('turnOnFailed/' + deviceId, JSON.stringify(failedMessage), function (error) {
+        if(error) {
+          console.log("publish turnOnFailed error:", error)
+        } else {
+          console.log("publish success, topic:", 'turnOnFailed/' + deviceId)
+        }
+      })
+    }, 1000)
+  }
+}
+
+client.on('message', function (topic, message, packet) {
+  var topicLevel1 = topic.split('/')[0]
+
+  switch (topicLevel1) {
+    case 'turnOn':
+      handleTurnOn(message)
+      break
+    default:
+      console.log("未知消息 topic:", message.toString())
+      break
+  }
 
 })
 
@@ -61,12 +132,12 @@ setInterval(function () {
     if(error) {
       console.log("publish deviceStatus error:", error)
     } else {
-      console.log("publish success, topic:", topic)
+      // console.log("publish success, topic:", topic)
     }
   })
 }, 1000)
 
-//10s后触发设备下线
+//10min后触发设备下线
 setTimeout(function () {
   var offlineMessage = {
     deviceId: DEVICEID,
@@ -82,7 +153,6 @@ setTimeout(function () {
       client.end()
     }
   })
-}, 10000)
-
+}, 10 * 60 * 1000)
 
 
